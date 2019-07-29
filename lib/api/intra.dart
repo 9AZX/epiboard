844434ee803d97e468f15a9fbdaf32bd022ea229
Jon Ihlas <jon.i@hotmail.fr>
2019-07-29 20:45:46 +0200

    Add manual cookie and making login work

--- lib/api/intra.dart
+++ lib/api/intra.dart
@@ -2,19 +2,28 @@ import 'package:http/http.dart' as http;
 
 class IntraApiService {
   final _baseUrl = "https://intra.epitech.eu";
+  final _headers = {
+    'Accept': '*/*',
+    'Connection': 'keep-alive',
+    'Cookie':
+        'user=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6Impvbi5paGxhcy1tb3Vlc2NhQGVwaXRlY2guZXUiLCJ0eiI6bnVsbCwiZXhwIjoxNTY0NzYwMDUzfQ.aTGCQ7xtYlOKr-g91vvDBZ4o9gmqPrIvxbF6c5qtsn0'
+  };
 
   Future<bool> autologinConnect(String link) async {
-    print(link);
-    final response = await http.get(link, headers: {
-      'Accept': '*/*',
-    });
+    final response = await http.get(link, headers: _headers);
 
     if (response.statusCode == 200) {
-      print("Okok");
       return true;
     } else {
       print(response.statusCode);
       return false;
     }
+  }
+
+  Future<bool> userInfo() async {
+    final response =
+        await http.get(_baseUrl + "/user/?format=json", headers: _headers);
+
+    print(response.body);
   }
 }
